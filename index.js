b2a5b5363e5b99c824fefc85adc2fb41adc2c747
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-02-16 22:02:18 +0200

    update index js and schema

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 const app = express();
-
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 const db = require("./database/db");
 
 const usersRouter = require("./routes/users"); //import users router
